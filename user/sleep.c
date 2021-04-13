9cfef26d2017091002b4f4424bda39b574d6fb65
before-Sunrise <unclejyj@gmail.com>
2021-04-13 22:24:39 +0800

    change to Makefile for lab util

--- user/sleep.c
+++ user/sleep.c
@@ -1,13 +1,14 @@
-#include "kernel/types.h"
-#include "user/user.h"
-int
-main(int argc, char *argv[])
+#include "kernel/types.h"
+#include "user/user.h"
+int main(int argc,char* argv[])
 {
-  if(argc != 2) {
-      fprintf(2, "please input one argument\n");
-      exit(1);
-  }
-  int time = atoi(argv[1]);
-  sleep(time);
-  exit(0);
+   if(argc<=1 || argc>2 )
+   {
+      fprintf(2,"Only 2 argument is needed!\n");
+      exit(1);
+   }
+   int ticks=atoi(argv[1]);
+   sleep(ticks);
+   exit(0);
 }
+
